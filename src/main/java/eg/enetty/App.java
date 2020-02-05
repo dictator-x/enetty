0dcd592756c0ab92f8c53d3725d2cea636763135
dictator-x <dictator.xwu@gmail.com>
2020-02-05 14:14:25 -0500

    initial project

---
+++ src/main/java/eg/enetty/App.java
@@ -0,0 +1,15 @@
+package eg.enetty;
+
+import io.netty.bootstrap.ServerBootstrap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        ServerBootstrap b = new ServerBootstrap();
+    }
+}
