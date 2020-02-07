8fdb921226ac923115338ae0a402fc0a1c37ff90
dictator-x <dictator.xwu@gmail.com>
2020-02-07 13:07:11 -0500

    finish simple server

--- src/main/java/eg/enetty/ServerApp.java
+++ src/main/java/eg/enetty/ServerApp.java
@@ -7,6 +7,8 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.logging.LogLevel;
 
 import eg.enetty.codec.*;
 import eg.enetty.handler.OrderServerProcessHandler;
@@ -18,14 +20,19 @@ public class ServerApp
 
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.channel(NioServerSocketChannel.class);
+
+        // Add log support
+        serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
         // If NioEventLoopGroup does not specify Executor, It will get
         // ThreadPerTaskExecutor as default.
+        // Jdk selector is instanced at this point.
         serverBootstrap.group(new NioEventLoopGroup());
 
         serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
             protected void initChannel(NioSocketChannel ch) throws Exception {
                 ChannelPipeline pipeline = ch.pipeline();
+                pipeline.addLast(new LoggingHandler(LogLevel.INFO));
                 pipeline.addLast(new OrderFrameDecoder());
                 pipeline.addLast(new OrderFrameEncoder());
                 pipeline.addLast(new OrderProtocolEncoder());
@@ -35,6 +42,12 @@ public class ServerApp
         });
 
         // bind method will execute eventloop
+        // register ServerBootstrapAcceptor into channel pipeline
+        // This will handle new Connection.
+        // Flow: SingleThreadEventExecutor.doStartThread()
+        //        -> SingleThreadEventExecutor.sub.run()
+        //            => NioEventLoop.run()
+        //        -> NioEventLoop.processSelectedKeys()
         ChannelFuture channelFuture = serverBootstrap.bind(8090).sync();
         channelFuture.channel().closeFuture().get();
     }
